5b372679f0d825b3bd3c90883a0f318dc227c42c
Guilherme Santos <guilherme.santos-tak@parceiros-kroton.com.br>
2022-09-13 18:26:11 -0300

    [TA004] - Uploading setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import email
+from setuptools import setup, find_packages
+
+
+setup(
+    name="python-dev-test",
+    version="1.0",
+    description='Implementing ETL process to analyze data',
+    author="Guilherme Santos",
+    author_email="guilherme-ads@live.com",
+    packages= ["src"],
+    include_package_data=True
+)
